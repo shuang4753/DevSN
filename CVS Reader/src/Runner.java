0911bc766d86bc9200b7c1ebb4494fc64fe30e84
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2018-03-19 09:31:51 -0400

    asd

---
+++ CVS Reader/src/Runner.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader; import java.io.IOException; import java.nio.charset.StandardCharsets; import java.nio.file.Files; import java.nio.file.Path; import java.nio.file.Paths; import java.util.ArrayList; import java.util.List; 
+//
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ CVS Reader/src/UtilitiesCVS.java
@@ -0,0 +1,10 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class UtilitiesCVS {
+
+	public static List<Book> readBooksFromCSV(String filename)
+	{
+		
+	}
+}
